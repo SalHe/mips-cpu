15c3d3f622c32a332333b297a2209c89f4b79fa5
SalHe <SalHe@qq.com>
2021-03-25 22:55:08 +0800

    add&test: IType forwarding

---
+++ test/cpu/tb-pipeline-forwarding-itype.fv.asm
@@ -0,0 +1,20 @@
+lui     $v0, 0x9988
+ori     $v0, $v0, 0x7766
+lui     $v1, 0x7766
+ori     $v1, 0x5544
+
+lui     $t0, 0x8000
+addiu   $t1, $t0, 0x8000
+addi    $t2, $t0, 0x8000
+andi    $t3, $t2, 0x8000
+xori    $t4, $t2, 0x8000
+
+# sw lw 配套测试 Test in pair
+sw      $t0, 0($zero)
+lw      $t5, 0($zero)
+
+# 99887766, 77665544, 80000000, 80008000, 7fff8000, 00008000, 7fff0000, 80000000, xxxxxxxx, xxxxxxxx 
+
+slti    $t6, $t0, 1
+addi    $t7, $t0, 1
+sltiu   $t7, $t0, 1
